ac95206cd9d8c3c755ca0e56fd59c02f2a9c0213
Eric McGinty <emcginty@envivid.com>
2011-10-27 17:45:51 -0700

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = 'fa23785b5bf35ec4ceed060ff527a9297856865306f21fe8a83b3e6f24654cb6f1f4667ea42ca721627f15f733e93a99588be0d4abd79533df285785c1911970'
